c7b0b7cbd955e3913056f3d567c15754ab303bf5
Karan PV <149856183+karanpv@users.noreply.github.com>
2024-01-06 08:39:09 +0530

    add comments to code

--- fortuneGenerator.js
+++ fortuneGenerator.js
@@ -1,4 +1,8 @@
+// random number generator
+
 const rndNumGenerator = num => Math.floor(Math.random() * num);
+
+// terms related to fortune
 
 const collectiveFortune = {
     sign: ['Cosmic Alignment', 'Celestial Harmony', 'Nebulous Insights', 'Astral Whispers', 'Stardust Serenity'],
@@ -6,7 +10,12 @@ const collectiveFortune = {
     advice: ['Trust inner guidance', 'Embrace changing tides', 'Cherish present moments', 'Seek hidden opportunities', 'Forgive, find peace']
 };
 
+
+// personal fortune array
+
 let personalFortune = [];
+
+// for loop to generate the personal fortune 
 
 for (let step in collectiveFortune) {
     let fortuneIdx = rndNumGenerator(collectiveFortune[step].length);
@@ -27,9 +36,13 @@ for (let step in collectiveFortune) {
     }
 }
 
+// function to turn array of fortune into a paragraph 
+
 const formatFortune = fortuneArr => {
     let format = fortuneArr.join('\n');
     console.log(format);
 }
+
+// calling the function 
 
 formatFortune(personalFortune);
