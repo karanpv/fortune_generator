2a97d53b90bf2751ecb59c3e226549423c8a798e
Karan PV <149856183+karanpv@users.noreply.github.com>
2024-01-06 08:13:58 +0530

    add terms related to fortune in object model

--- fortuneGenerator.js
+++ fortuneGenerator.js
@@ -1,1 +1,9 @@
 const rndNumGenerator = num => Math.floor(Math.random() * num);
+
+const collectiveFortune = {
+    sign: ['Cosmic Alignment', 'Celestial Harmony', 'Nebulous Insights', 'Astral Whispers', 'Stardust Serenity'],
+    fortune: ['Joyful surprises await', 'Success beckons courageously', 'Harmony brings prosperity', 'Love transforms everything', 'Challenges test resilience', 'Caution avoids pitfalls', 'Loss sparks growth'],
+    advice: ['Trust inner guidance', 'Embrace changing tides', 'Cherish present moments', 'Seek hidden opportunities', 'Forgive, find peace']
+};
+
+let personalFortune = [];
